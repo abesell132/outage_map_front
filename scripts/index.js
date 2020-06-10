b48f79b9a9955a61105bbd920ef4279c28b694fb
unknown <abesell132@gmail.com>
2020-06-10 18:22:34 -0400

    asdf

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,14 @@
 function initMap() {
   load_map();
-  //   get_township_data().then((res) => {
-  //     console.log(res);
-  //     get_zipcode_data().then((res2) => {
-  //       console.log(res2);
-  //       get_county_data().then((res3) => {
-  //         console.log(res3);
-  //         resolve();
-  //       });
-  //     });
-  //   });
-  console.log("Hello World");
+  get_township_data().then((res) => {
+    console.log(res);
+    get_zipcode_data().then((res2) => {
+      console.log(res2);
+      get_county_data().then((res3) => {
+        console.log(res3);
+      });
+    });
+  });
 }
 
 function start_map_population() {
--- scripts/load_outage_data.js
+++ scripts/load_outage_data.js
@@ -12,18 +12,27 @@ function load_outage_data() {
 }
 
 function get_township_data() {
-  axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_township.json").then((res) => {
-    console.log(res);
+  return new Promise((resolve, reject) => {
+    axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_township.json").then((res) => {
+      console.log(res);
+      resolve();
+    });
   });
 }
 
-function get_zipcode_data(callback) {
-  axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_zipcode.json").then((res) => {
-    console.log(res);
+function get_zipcode_data() {
+  return new Promise((resolve, reject) => {
+    axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_zipcode.json").then((res) => {
+      resolve();
+      console.log(res);
+    });
   });
 }
-function get_county_data(callback) {
-  axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_county.json").then((res) => {
-    console.log(res);
+function get_county_data() {
+  return new Promise((resolve, reject) => {
+    axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_county.json").then((res) => {
+      console.log(res);
+      resolve();
+    });
   });
 }

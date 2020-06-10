f55f274d6be971cc698af9aac248f95d295a91a5
unknown <abesell132@gmail.com>
2020-06-10 18:10:23 -0400

    Asdf

--- scripts/load_outage_data.js
+++ scripts/load_outage_data.js
@@ -1,25 +1,34 @@
 function load_outage_data() {
   return new Promise((resolve, reject) => {
-    get_township_data(resolve);
+    load_data_files(resolve);
   });
 }
 
-function get_township_data(callback) {
+function load_data_files(callback) {
+  get_township_data().then((res) => {
+    console.log(res);
+    get_zipcode_data().then((res) => {
+      console.log(res);
+      get_county_data().then((res) => {
+        console.log(res);
+        callback();
+      });
+    });
+  });
+}
+function get_township_data() {
   axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_township.json").then((res) => {
     console.log(res);
-    get_zipcode_data(callback);
   });
 }
 
 function get_zipcode_data(callback) {
   axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_zipcode.json").then((res) => {
     console.log(res);
-    get_zipcode_data(callback);
   });
 }
 function get_county_data(callback) {
   axios.get("https://www.uppco.com/_uppco-outage-files/sorted/sorted_uppco_county.json").then((res) => {
     console.log(res);
-    callback();
   });
 }

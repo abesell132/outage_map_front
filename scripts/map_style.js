81e677cf8b38893cb4571412521d14fe710f289f
unknown <abesell132@gmail.com>
2020-06-11 10:56:37 -0400

    Asdf

--- scripts/map_style.js
+++ scripts/map_style.js
@@ -64,48 +64,48 @@ let map_style = [
   },
 ];
 
-// var myDirectory = "https://uppco.com/wp-content/themes/bb-theme-child";
-// var images = myDirectory + "/img/m";
+var myDirectory = "https://uppco.com/wp-content/themes/bb-theme-child";
+var images = myDirectory + "/img/m";
 
-// var cluster_styles = [
-//   {
-//     url: images + "1.png",
-//     textColor: "white",
-//     textSize: 12,
-//     height: 53,
-//     width: 53,
-//     anchor: [0, 0],
-//   },
-//   {
-//     url: images + "2.png",
-//     textColor: "black",
-//     textSize: 13,
-//     height: 56,
-//     width: 56,
-//     anchor: [0, 0],
-//   },
-//   {
-//     url: images + "3.png",
-//     textColor: "white",
-//     textSize: 13,
-//     height: 59,
-//     width: 59,
-//     anchor: [0, 0],
-//   },
-//   {
-//     url: images + "4.png",
-//     textColor: "white",
-//     textSize: 15,
-//     height: 71,
-//     width: 71,
-//     anchor: [0, 0],
-//   },
-//   {
-//     url: images + "5.png",
-//     textColor: "white",
-//     textSize: 15,
-//     height: 71,
-//     width: 71,
-//     anchor: [0, 0],
-//   },
-// ];
+var cluster_styles = [
+  {
+    url: images + "1.png",
+    textColor: "white",
+    textSize: 12,
+    height: 53,
+    width: 53,
+    anchor: [0, 0],
+  },
+  {
+    url: images + "2.png",
+    textColor: "black",
+    textSize: 13,
+    height: 56,
+    width: 56,
+    anchor: [0, 0],
+  },
+  {
+    url: images + "3.png",
+    textColor: "white",
+    textSize: 13,
+    height: 59,
+    width: 59,
+    anchor: [0, 0],
+  },
+  {
+    url: images + "4.png",
+    textColor: "white",
+    textSize: 15,
+    height: 71,
+    width: 71,
+    anchor: [0, 0],
+  },
+  {
+    url: images + "5.png",
+    textColor: "white",
+    textSize: 15,
+    height: 71,
+    width: 71,
+    anchor: [0, 0],
+  },
+];
